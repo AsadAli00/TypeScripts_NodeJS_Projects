cdb65543c13069e24609c4af79a2ec7907677b5a
Engr Asad <46523348+AsadAli00@users.noreply.github.com>
2024-03-24 13:30:24 +0500

    typescript initializing, simple calculator, Hello World in TypeScript

---
+++ Simple-Calculater/index.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { name: "Number1", message: "Enter Your first number: ", type: "number" },
+    { name: "Number2", message: " Enter your Second Number : ", type: "number" },
+    { name: "Operator", message: " Select one of the Operator to perform operation", type: "list", choices: ["Addition", "Subtraction", "Multiplication", "division"] }
+]);
+//Condition Statement
+if (answer.Operator === "Addition") {
+    console.log("Your Answer is : ", answer.Number1 + answer.Number2);
+}
+else if (answer.Operator === "Subtraction") {
+    console.log("Your Answer is : ", answer.Number1 - answer.Number2);
+}
+else if (answer.Operator === "Multiplication") {
+    console.log("Your Answer is : ", answer.Number1 * answer.Number2);
+}
+else if (answer.Operator === "division") {
+    console.log("Your Answer is : ", answer.Number1 / answer.Number2);
+}
+else {
+    console.log("Wrong Operator Selection");
+}
